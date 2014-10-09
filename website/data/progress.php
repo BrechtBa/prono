92a28adf67e8c2e23a35b1e51dd03ce6ca07a915
brecht <brecht.baeten@gmail.com>
2014-10-09 18:39:00 +0200

    added data handling

--- website/data/progress.php
+++ website/data/progress.php
@@ -1,4 +1,56 @@
 <?php
+///////////////////////////////////////////////////////////////////////////////
+// progress invullen
+///////////////////////////////////////////////////////////////////////////////
+if(array_key_exists('knockout_teams',$_POST)){
+	// in pronostiek invullen
+	if($prono){
+
+		for($i=1;$i<=8;$i++){
+			$team = $_POST['quarter_team'.$i];
+			$query = "UPDATE wk_users SET quarter_team".$i."=$team WHERE id=$userid";
+			$result = mysql_query($query) or die('Error: ' . mysql_error());
+		}
+		for($i=1;$i<=4;$i++){
+			$team = $_POST['semi_team'.$i];
+			$query = "UPDATE wk_users SET semi_team".$i."=$team WHERE id=$userid";
+			$result = mysql_query($query) or die('Error: ' . mysql_error());
+		}
+		for($i=1;$i<=2;$i++){
+			$team = $_POST['final_team'.$i];
+			$query = "UPDATE wk_users SET final_team".$i."=$team WHERE id=$userid";
+			$result = mysql_query($query) or die('Error: ' . mysql_error());
+		}
+		for($i=1;$i<=1;$i++){
+			$team = $_POST['winner'];
+			$query = "UPDATE wk_users SET winner=$team WHERE id=$userid";
+			$result = mysql_query($query) or die('Error: ' . mysql_error());
+		}
+		
+		///
+		if($_POST['number']==''){
+			$goals = -1;
+		}
+		else{
+			$goals = $_POST['number'];
+		}
+		$query = "UPDATE wk_users SET total_goals=$goals WHERE id=$userid";
+		$result = mysql_query($query) or die('Error: ' . mysql_error());
+		
+		///
+		if($_POST['ranking']==''){
+			$ranking = -1;
+		}
+		else{
+			$ranking = $_POST['ranking'];
+		}
+		$query = "UPDATE wk_users SET home_team_ranking=$ranking WHERE id=$userid";
+		$result = mysql_query($query) or die('Error: ' . mysql_error());
+		//echo "<meta http-equiv='refresh' content='0; URL=index.php?page=$page'>";
+	}
+}
+
+
 
 ///////////////////////////////////////////////////////////////////////////////
 // Display Progress
