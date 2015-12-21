7965bec2a68c8cc9622a44caf2611bb49d9a6fe3
brecht <brecht.baeten@gmail.com>
2015-12-21 22:17:43 +0100

    made changes to move to final domain and added some theming

--- www/scripts/app.js
+++ www/scripts/app.js
@@ -8,13 +8,12 @@
 	var app = document.querySelector('#app');
 
 	// Sets app default base URL
-	app.baseUrl = '/prono/';
+	app.baseUrl = '';
 	if (window.location.port === '') {  // if production
 		// Uncomment app.baseURL below and
 		// set app.baseURL to '/your-pathname/' if running from folder in production
 		// app.baseUrl = '/polymer-starter-kit/';
 	}
-
 	app.displayInstalledToast = function() {
 		// Check to make sure caching is actually enabled—it won't be in the dev environment.
 		if (!Polymer.dom(document).querySelector('platinum-sw-cache').disabled) {
