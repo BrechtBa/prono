c9d34f5794218ec94fb30709792698cbac73bd6c
brecht <brecht.baeten@gmail.com>
2016-04-30 18:35:24 +0200

    updated matches list and groupstage to the new api

--- www/backend/api/utils.py
+++ www/backend/api/utils.py
@@ -3,6 +3,6 @@ import datetime
 
 def unixtimestamp(dt=None):
 	if dt == None:
-		return (datetime.datetime.utcnow()-datetime.datetime(1970,1,1)).total_seconds()
+		return int( (datetime.datetime.utcnow()-datetime.datetime(1970,1,1)).total_seconds() )
 	else:
-		return (dt-datetime.datetime(1970,1,1)).total_seconds()
+		return int( (dt-datetime.datetime(1970,1,1)).total_seconds() )
