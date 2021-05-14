8bdec4a1b793405c1082a5db353eca9dba7c793b
Brecht Baeten <brecht.baeten@enervalis.com>
2021-05-14 23:00:24 +0200

    added basic layout

--- app-react/src/App.js
+++ app-react/src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import PronoLayout from './Layout.js';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+
+
+const redTheme = createMuiTheme({
+  palette: {
+    background: {
+      default: "#ab0000",
+      gradient: "linear-gradient(#DD0000, #440000)",
+      paper: "#ffffff"
+    },
+    primary: {
+      main: "#ffffff",
+      light: "rgba(55, 55, 55, 1)",
+      dark: "rgba(10, 10, 10, 1)",
+      contrastText: "#333333",
+    },
+    secondary: {
+      main: "rgba(229, 229, 229, 1)",
+      light: "rgba(220, 220, 220, 1)",
+      dark: "rgba(200, 200, 200, 1)",
+      contrastText: "rgba(33, 33, 33, 1)",
+    },
+    text: {
+      primary: "#333333",
+      secondary: "#333333",
+      disabled: "#555555",
+      hint: "#333333",
+    },
+  },
+
+  overrides: {
+    MuiSlider: {
+      colorPrimary: {
+        color: "rgba(240, 240, 240, 1)",
+      },
+    },
+  }
+
+});
+
 
 function App() {
+
+  const isAuthenticated = true;
+  const user = {
+    displayName: 'BB',
+    permissions: {
+      admin: true
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{height: '100%'}}>
+      <ThemeProvider theme={redTheme}>
+        <PronoLayout isAuthenticated={isAuthenticated} user={user}/>
+      </ThemeProvider>
     </div>
   );
+
 }
 
 export default App;
---
+++ app-react/src/Layout.js
@@ -0,0 +1,252 @@
+import React, { useState } from 'react';
+import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
+
+import { createStyles, Theme, makeStyles } from '@material-ui/core/styles';
+import SwipeableDrawer from '@material-ui/core/SwipeableDrawer';
+import AppBar from '@material-ui/core/AppBar';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Toolbar from '@material-ui/core/Toolbar';
+
+import Typography from '@material-ui/core/Typography';
+import Avatar from '@material-ui/core/Avatar';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import Divider from '@material-ui/core/Divider';
+
+import IconButton from '@material-ui/core/IconButton';
+
+import MenuIcon from '@material-ui/icons/Menu';
+
+
+const drawerWidth = 320;
+
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      display: 'flex',
+      minHeight: '100%',
+      background: theme.palette.background.gradient,
+    },
+
+    drawer: {
+      width: drawerWidth,
+      flexShrink: 0,
+    },
+
+    drawerPaper: {
+      width: drawerWidth,
+    },
+
+    content: {
+      flexGrow: 1,
+      padding: theme.spacing(3),
+    },
+
+    user: {
+      backgroundColor: theme.palette.background.default,
+    },
+
+    navigationItem: {
+      textDecoration: 'none',
+      color: theme.palette.primary.contrastText,
+      width: 'auto',
+    },
+
+    version: {
+      marginLeft: '5px',
+      marginTop: '30px'
+    },
+  }),
+
+
+);
+
+
+function User(props) {
+
+  const user = props.user
+
+  const classes = useStyles();
+  return (
+    <div className={classes.user}>
+      <Avatar alt={user.displayName} src={user.avatar} className={classes.large} />
+    </div>
+  );
+}
+
+
+function Navigation(props) {
+
+  const isAdmin = props.isAdmin;
+  const onNavigation = props.onNavigation;
+
+  console.log(isAdmin)
+
+  const links = [{
+    title: 'Rangschikking',
+    path: 'ranking'
+  }, {
+    title: 'Prono',
+    path: 'prono'
+  }, {
+    title: 'Resultaten',
+    path: 'results'
+  }, {
+    title: 'Regels',
+    path: 'rules'
+  }]
+
+  const adminLinks = [{
+    title: 'Users',
+    path: 'users'
+  }, {
+    title: 'Settings',
+    path: 'settings'
+  }, {
+    title: 'Teams',
+    path: 'teams'
+  }, {
+    title: 'Matches',
+    path: 'matches'
+  }, {
+    title: 'Groupstage',
+    path: 'groupstage'
+  }, {
+    title: 'Knockoutstage',
+    path: 'kockoutstage'
+  }]
+  const classes = useStyles();
+
+  const navigationItem = (link, index) => {
+    return (
+      <Link key={index} to={link.path} className={classes.navigationItem} onClick={onNavigation()}>
+        <ListItem button key={link.title}>
+          <ListItemText primary={link.title} />
+        </ListItem>
+      </Link>
+    );
+  }
+
+  return (
+    <div>
+      <List>
+        {links.map((link, index) => navigationItem(link, index))}
+      </List>
+
+      <Divider/>
+      {isAdmin &&
+        adminLinks.map((link, index) => navigationItem(link, index))
+      }
+      {isAdmin &&
+        <Divider/>
+      }
+    </div>
+  );
+}
+
+
+function Version() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.version}>v2.0</div>
+   )
+}
+
+function PronoLayout(props) {
+  const isAuthenticated = props.isAuthenticated;
+  const user = props.user;
+
+  const classes = useStyles();
+  const [navigationOpen, setNavigationOpen] = useState(false);
+
+  const toggleNavigation = (open) => (event) => {
+    if(open === undefined){
+      setNavigationOpen(!navigationOpen);
+    }
+    else{
+      setNavigationOpen(open);
+    }
+  }
+
+  if(isAuthenticated){
+    return (
+      <BrowserRouter>
+        <div className={classes.root}>
+          <CssBaseline />
+          <AppBar position="fixed" className={classes.appBar}>
+            <Toolbar style={{display: "flex", paddingRight: "12px"}}>
+
+                <IconButton edge="start" color="inherit" aria-label="open drawer" onClick={toggleNavigation()}>
+                  <MenuIcon/>
+                </IconButton>
+
+              <Typography variant="h6" style={{flexGrow: 1}}  noWrap>
+                  Prono
+              </Typography>
+            </Toolbar>
+          </AppBar>
+
+          <SwipeableDrawer anchor="left" className={classes.drawer} classes={{paper: classes.drawerPaper}}
+           open={navigationOpen} onClose={toggleNavigation(false)} onOpen={toggleNavigation(true)}>
+            <User user={user}/>
+            <Navigation isAdmin={user.permissions.admin} onNavigation={toggleNavigation}/>
+            <Version/>
+          </SwipeableDrawer>
+
+          <main className={classes.content}>
+            <Toolbar />
+            <Switch>
+              <Route path="/ranking">
+                Ranking
+              </Route>
+              <Route path="/prono">
+                Prono
+              </Route>
+              <Route path="/results">
+                Results
+              </Route>
+              <Route path="/rules">
+                Rules
+              </Route>
+
+
+              <Route path="/users">
+                Users
+              </Route>
+              <Route path="/settings">
+                Users
+              </Route>
+              <Route path="/teams">
+                Users
+              </Route>
+              <Route path="/matches">
+                Users
+              </Route>
+              <Route path="/hroupstage">
+                hroupstage
+              </Route>
+              <Route path="/knockoutstage">
+                Users
+              </Route>
+
+              <Route path="/">
+                Ranking
+              </Route>
+
+            </Switch>
+          </main>
+
+        </div>
+      </BrowserRouter>
+    );
+  }
+  else {
+    return null
+  }
+
+}
+
+export default PronoLayout;
