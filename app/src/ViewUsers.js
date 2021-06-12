ee08ed3d426e35a6a74fcc620fe2a837d961c026
brecht <brecht.baeten@gmail.com>
2021-06-12 09:21:32 +0200

    small change to admin layout

--- app/src/ViewUsers.js
+++ app/src/ViewUsers.js
@@ -36,14 +36,20 @@ function User(props){
     <div style={{marginBottom: '5px'}}>
 
       <Paper style={{padding: '10px'}}>
-        <div style={{display: 'flex', alignItems: 'center'}}>
-          <Avatar alt={user.displayName} src={user.profilePicture} style={{height: '50px', width: '50px', marginRight: '20px'}}>{getFirstLetter(user)}</Avatar>
-          <div style={{width: '150px'}}>{user.displayName}</div>
-          <div><Checkbox checked={user.active} onChange={(e) => api.updateActive(user, !user.active)}/> Active</div>
-          <div><Checkbox checked={user.paid} onChange={(e) => api.updatePaid(user, !user.paid)}/> Paid</div>
-          <div><Checkbox checked={user.showPoints} onChange={(e) => api.updateShowPoints(user, !user.showPoints)}/> Show points</div>
-          <div><Checkbox checked={user.permissions.editor} onChange={(e) => api.updatePermissionEditor(user, !user.permissions.editor)}/> Editor</div>
-          <div><Button onClick={() => setPronoDialogOpen(true)}>Edit prono</Button></div>
+        <div style={{display: 'flex', alignItems: 'center', flexWrap: 'wrap'}}>
+          <div style={{display: 'flex', alignItems: 'center', marginRight: '10px', marginBottom: '5px'}}>
+            <Avatar alt={user.displayName} src={user.profilePicture} style={{height: '50px', width: '50px', marginRight: '20px'}}>{getFirstLetter(user)}</Avatar>
+            <div style={{width: '150px'}}>{user.displayName}</div>
+          </div>
+          <div style={{display: 'flex', alignItems: 'center', marginRight: '10px', marginBottom: '5px'}}>
+            <div><Checkbox checked={user.active} onChange={(e) => api.updateActive(user, !user.active)}/> Active</div>
+            <div><Checkbox checked={user.paid} onChange={(e) => api.updatePaid(user, !user.paid)}/> Paid</div>
+            <div><Checkbox checked={user.showPoints} onChange={(e) => api.updateShowPoints(user, !user.showPoints)}/> Points</div>
+            <div><Checkbox checked={user.permissions.editor} onChange={(e) => api.updatePermissionEditor(user, !user.permissions.editor)}/> Editor</div>
+          </div>
+          <div style={{display: 'flex', alignItems: 'center', marginRight: '10px', marginBottom: '5px'}}>
+            <div><Button onClick={() => setPronoDialogOpen(true)}>Edit prono</Button></div>
+          </div>
         </div>
       </Paper>
 
@@ -75,7 +81,8 @@ function ViewUsers(props) {
     <div className={classes.root}>
       <h2 style={{color: '#ffffff'}}>Users</h2>
 
-      <div><Checkbox checked={filterActive} onChange={(e) => setFilterActive(!filterActive)}/> hide inactive</div>
+      <div><Checkbox checked={filterActive} onChange={(e) => setFilterActive(!filterActive)}/>Hide inactive</div>
+
       <div>
         {users.filter((user) => !filterActive || user.active).map((user) => (
           <User key={user.key} user={user} ranking={user.rank}/>
